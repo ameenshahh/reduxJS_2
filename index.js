407e71680c0c0e99b122bbdc173cbbc9f0280241
Skill Actz <info@trinityskillworks.com>
2021-10-05 23:08:23 +0530

    created constants and action

---
+++ index.js
@@ -0,0 +1,13 @@
+//constants
+const BUY_CAKE = 'BUY_CAKE'
+
+//actions
+
+
+//action creator
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
