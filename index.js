d97d2c848e046e19e43263ac16931eba77f28691
Skill Actz <info@trinityskillworks.com>
2021-10-05 23:22:40 +0530

    created reducers

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function buyCake(){
         info: 'First redux action'
     }
 }
+
+/* reducers accepts previous state and actions and returns the new state based on the action 
+given */
+
+const initialState = {
+    numOfCakes : 10
+}
+
+
+const reducer =(state=initialState,action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state, //this is done to make sure that the other state ppts are not changed but only numOfCakes
+            numOfCakes : state.numOfCakes - 1
+        }
+
+        default : return state
+    }
+}
